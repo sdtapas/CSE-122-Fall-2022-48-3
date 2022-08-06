d9a4566a12374253b078965682b835359cf5a1da
Tapas sutrodhar <106900770+sdtapas@users.noreply.github.com>
2022-08-06 12:00:15 +0100

    Add files via upload

---
+++ Lab-01/1.423_49-08.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int weight;
+    cin>>weight;
+    if(weight%2==0&& weight!=0)
+        cout<<"YES";
+    else
+        cout<<"NO";
+    return 0;
+}
